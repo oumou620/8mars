14337815a8df9432b72be13cd92551d2dc4e594e
oumou620 <diallooumouhawa703@gmail.com>
2025-03-03 11:16:58 +0000

    Switch to HashRouter for better GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -9,7 +9,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/8mars">
+    <HashRouter>
       <ThemeProvider>
         <LanguageProvider>
           <AuthProvider>
@@ -17,6 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </AuthProvider>
         </LanguageProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
